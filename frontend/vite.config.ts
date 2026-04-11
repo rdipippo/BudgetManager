0048c2413f2af8d552a94b3095589cfa13962ad9
Richard DiPippo <rdipippo@iMac.lan>
2026-04-11 11:35:57 -0400

    make deployed app work

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    base: process.env.VITE_BASE_PATH || '/',
     plugins: [react()],
     resolve: {
       alias: {
